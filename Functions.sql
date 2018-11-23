f10f79b5551431ed890c5391fde7d5d5a4aa9e88
Diogo Sobral <diogosobral98@gmail.com>
2018-11-23 18:49:38 +0000

    sql semi pronto

--- Functions.sql
+++ Functions.sql
@@ -11,4 +11,25 @@ END $$
 
 SELECT total_gasto(2);
 
+DELIMITER $$
+CREATE FUNCTION total_bilhetes_vendidos(d_inicio DATETIME,d_fim DATETIME)
+	   RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (SELECT count(b)
+			FROM bilhete as b
+			WHERE b.data_aquisicao >= d_inicio
+				AND b.data_aquisicao <= d_fim);
+END $$
+
+
+DELIMITER $$
+CREATE FUNCTION total_faturado(d_inicio DATETIME,d_fim DATETIME)
+	   RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (SELECT sum(b.preco)
+			FROM bilhete as b
+			WHERE b.data_aquisicao >= d_inicio
+				AND b.data_aquisicao <= d_fim);
+END $$
+
 
--- Procedures.sql
+++ Procedures.sql
@@ -5,7 +5,7 @@
 DELIMITER $$
 CREATE PROCEDURE historico_viagens(IN id_cliente INT)
 BEGIN
-	SELECT v.data_partida, eo.nome, v.data_chegada, ed.nome
+	SELECT v.data_partida AS 'Data de Partida', eo.nome AS 'Origem', v.data_chegada  AS 'Data de Chegada', ed.nome AS 'Destino'
     FROM bilhete AS b INNER JOIN viagem AS v
 					  ON b.viagem = v.id_viagem
 							INNER JOIN estacao AS eo
@@ -22,7 +22,7 @@ CALL historico_viagens(1);
 DELIMITER $$
 CREATE PROCEDURE detalhes_viagem(IN id_bilhete INT)
 BEGIN
-	SELECT v.data_partida, eo.nome, v.data_chegada, ed.nome, v.duracao, b.preco, b.classe, b.numero
+	SELECT v.data_partida AS 'Data de Partida', eo.nome AS 'Origem', v.data_chegada AS 'Data de Chegada', ed.nome AS 'Destino', v.duracao AS 'Duração', b.preco AS 'Preço', b.classe AS 'Classe', b.numero AS 'Lugar'
     FROM bilhete AS b INNER JOIN viagem AS v
 					  ON b.viagem = v.id_viagem
 							INNER JOIN estacao AS eo
@@ -39,7 +39,7 @@ CALL detalhes_viagem(2);
 DELIMITER $$
 CREATE PROCEDURE viagens_between(IN id_estacao_o INT, id_estacao_d INT, data_inicio DATETIME, data_fim DATETIME)
 BEGIN
-	SELECT v.id_viagem, v.data_partida, v.data_chegada, v.duracao
+	SELECT v.id_viagem AS 'ID', v.data_partida AS 'Data de Partida', v.data_chegada AS 'Data de Chegada', v.duracao AS 'Duração'
     FROM viagem AS v
 	WHERE v.data_partida > data_inicio AND v.data_chegada < data_fim
 		  AND v.origem = id_estacao_o AND v.destino = id_estacao_d;
@@ -52,7 +52,7 @@ CALL viagens_between(1, 2, '2018-01-01 00:00:00', '2018-12-01 00:00:00');
 DELIMITER $$
 CREATE PROCEDURE lugares_livres(IN id_viagem INT)
 BEGIN
-	SELECT l.classe, l.numero
+	SELECT l.classe AS 'Classe', l.numero AS 'Lugar'
 	FROM Viagem AS v INNER JOIN Comboio AS c
 					 ON v.comboio = c.id_comboio
 						INNER JOIN Lugar AS l
@@ -74,10 +74,13 @@ CALL lugares_livres(1);
 DELIMITER $$
 CREATE PROCEDURE horario_partida_estacao(IN id_estacao INT)
 BEGIN
-	SELECT v.data_partida
+	SELECT v.data_partida AS 'Data de Partida', d.nome AS 'Destino'
     FROM estacao AS e INNER JOIN viagem AS v
 					  ON e.id_estacao = v.origem
-	WHERE e.id_estacao = id_estacao;
+                      INNER JOIN Estacao AS D
+                      ON v.destino = d.id_estacao
+	WHERE e.id_estacao = id_estacao
+		AND v.data_partida > NOW();
 END $$
 
 CALL horario_partida_estacao(1);
@@ -87,10 +90,13 @@ CALL horario_partida_estacao(1);
 DELIMITER $$
 CREATE PROCEDURE horario_chegada_estacao(IN id_estacao INT)
 BEGIN
-	SELECT v.data_chegada
+	SELECT v.data_chegada AS 'Data de Chegada', d.nome AS 'Origem'
     FROM estacao AS e INNER JOIN viagem AS v
 					  ON e.id_estacao = v.destino
-	WHERE e.id_estacao = id_estacao;
+                      INNER JOIN Estacao AS D
+                      ON v.destino = d.id_estacao
+	WHERE e.id_estacao = id_estacao
+		AND v.data_chegada > NOW();
 END $$
 
 CALL horario_chegada_estacao(2);
@@ -103,7 +109,7 @@ CALL horario_chegada_estacao(2);
 DELIMITER $$
 CREATE PROCEDURE viagens_comboio_between(id_comboio INT, data_inicio DATETIME, data_fim DATETIME)
 BEGIN
-	SELECT eo.nome AS 'Origem', v.data_partida, ed.nome AS 'Destino', v.data_chegada 
+	SELECT eo.nome AS 'Origem', v.data_partida AS 'Data de Partida', ed.nome AS 'Destino', v.data_chegada AS 'Data de Chegada'
     FROM viagem AS v INNER JOIN comboio AS c
 					 ON v.comboio = c.id_comboio
                      INNER JOIN estacao AS eo
@@ -132,3 +138,12 @@ END $$
 
 CALL clientes_between_estacoes(1, 2, '2018-01-01 00:00:00', '2018-12-01 00:00:00');
 
+
+DELIMITER $$
+CREATE PROCEDURE clientes_na_viagem(IN id_viagemp INT)
+BEGIN
+	SELECT c.nome AS Nome, c.id_cliente AS ID
+		FROM Bilhete b, Cliente c
+        WHERE b.viagem = id_viagemp
+			AND b.cliente = c.id_cliente;
+END $$
