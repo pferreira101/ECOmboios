6967b1aacb61212820a05d00944d89a4d9ca3166
Pedro Moreira <pedrombm19@gmail.com>
2018-11-25 15:40:14 +0000

    Minor fix prints Físico

--- Views.sql
+++ Views.sql
@@ -1,10 +1,12 @@
 
-CREATE VIEW proxima_viagens AS SELECT o.nome as Origem, v.data_partida as 'Data Partida', d.nome as Destino, v.data_chegada as 'Data Chegada' FROM Viagem as V INNER JOIN Estacao as O
-	ON v.origem = o.id_estacao
-    INNER JOIN Estacao as D
-		ON v.destino = d.id_estacao
-	WHERE v.data_partida >= now()
-    ORDER BY v.data_partida ASC;
+CREATE VIEW proxima_viagens AS 
+SELECT o.nome as 'Origem', v.data_partida as 'Data Partida', d.nome as 'Destino', v.data_chegada as 'Data Chegada' 
+FROM viagem as v INNER JOIN estacao as o
+				 ON v.origem = o.id_estacao
+				 INNER JOIN Estacao as d
+				 ON v.destino = d.id_estacao
+WHERE v.data_partida >= now()
+ORDER BY v.data_partida ASC;
 
 
 
