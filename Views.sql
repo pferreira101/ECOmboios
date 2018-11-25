0eb7ed0fbe4e351bc616024df309b0a33a2ea832
Diogo Sobral <diogosobral98@gmail.com>
2018-11-25 17:47:40 +0000

    vistas para ler

--- Views.sql
+++ Views.sql
@@ -9,6 +9,24 @@ WHERE v.data_partida >= now()
 ORDER BY v.data_partida ASC;
 
 
+CREATE VIEW numero_passagueiro_naviagem AS
+SELECT v.id_viagem AS 'ID Viagem', count(v.id_viagem) AS 'Número de Bilhetes Vendidos', v.data_partida AS 'Data de Partida'
+		FROM Viagem v , Bilhete b 
+        WHERE v.id_viagem = b.viagem
+        GROUP BY v.id_viagem
+        ORDER BY v.data_partida;
+
+CREATE VIEW numero_passagueiros_estacoes AS
+SELECT O.nome AS Origem , D.nome AS Destino, COUNT(b.id_bilhete) AS 'Número de Passageiros'
+FROM Bilhete AS B RIGHT JOIN Viagem AS V
+					ON B.viagem = V.id_viagem
+                    INNER JOIN Estacao AS O
+                    ON V.origem = O.id_estacao
+                    INNER JOIN Estacao AS D
+                    ON V.destino = D.id_estacao
+GROUP BY O.id_estacao , D.id_estacao
+ORDER BY O.nome ASC;
+
 
 CREATE VIEW estacoes_existentes AS
 SELECT nome
