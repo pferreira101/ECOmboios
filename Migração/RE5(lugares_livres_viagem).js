43a375de23800b61d872cf2f6428b4094ed9098c
PedroFerr101 <pferreira59@gmail.com>
2019-01-15 11:06:22 +0000

    re5

---
+++ Migração/RE5(lugares_livres_viagem).js
@@ -0,0 +1,28 @@
+ var id_viagem = 1;
+
+ function get_results (result) {
+    print(tojson(result));
+ }
+
+ db.Viagem.aggregate([
+ 	{
+ 		$match: {
+ 			"_id": id_viagem
+ 		}
+ 	},
+ 	{
+ 		$unwind: 
+ 			"$bilhetes"
+ 	},
+ 	{
+ 		$match: {
+ 			"bilhetes.data_aquisicao": null
+ 		}
+ 	}, 
+ 	{
+ 		$project: {
+ 			"bilhetes.classe":1,
+ 			"bilhetes.numero":1
+ 		}
+ 	}
+ ]).forEach(get_results)
