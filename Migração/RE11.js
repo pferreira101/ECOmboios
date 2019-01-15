9ce30d00bbd5454ace3e33c54b9ecd8b75080aa0
PedroFerr101 <pferreira59@gmail.com>
2019-01-15 10:07:35 +0000

    update re11

--- Migração/RE11.js
+++ Migração/RE11.js
@@ -1,22 +1,32 @@
 var data_inicio = "2018-11-14T00:00:00Z";
 var data_fim = "2018-11-16T00:00:00Z";
 
-db.Viagem.aggregate([ 
-	{ 
-		$match: { 
-				"bilhetes.data_aquisicao": {
-					$gte: ISODate(data_inicio), 
-					$lte: ISODate(data_fim)}
-				}
-	}, 
-	{
-		$unwind: "$bilhetes"
-	}, 
-	{ 
-		$group: { 
-			_id: null, 
-			num_bilhetes: { 
-				$sum: 1 } 
-			} 
-		}
-	])
+function get_results (result) {
+    print(tojson(result));
+}
+
+
+db.Viagem.aggregate(
+	[ 
+		{ 
+			$match: { 
+					"bilhetes.data_aquisicao": 
+						{
+							$gte: ISODate(data_inicio), 
+							$lte: ISODate(data_fim)
+						}
+					}
+		}, 
+		{
+			$unwind: "$bilhetes"
+		}, 
+		{ 
+			$group: 
+				{ 
+					_id: null, 
+					num_bilhetes: { 
+					$sum: 1 } 
+				} 
+			}
+	]
+).forEach(get_results)
